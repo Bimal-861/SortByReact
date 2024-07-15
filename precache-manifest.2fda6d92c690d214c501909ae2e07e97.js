19771cd002ff6d1bb3ceee9abf9a60f2f5f478f2
Bimal-861 <115387906+Bimal-861@users.noreply.github.com>
2024-07-15 23:11:36 +0530

    Updates

---
+++ precache-manifest.2fda6d92c690d214c501909ae2e07e97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14567e482abe960a47fd2f7ee738018",
+    "url": "/SortByReact/index.html"
+  },
+  {
+    "revision": "146e51acf10d9d705418",
+    "url": "/SortByReact/static/css/2.4e15b5ff.chunk.css"
+  },
+  {
+    "revision": "f2255c1b5c681ab48fbe",
+    "url": "/SortByReact/static/css/main.1138dfe6.chunk.css"
+  },
+  {
+    "revision": "146e51acf10d9d705418",
+    "url": "/SortByReact/static/js/2.161d19a4.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/SortByReact/static/js/2.161d19a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2255c1b5c681ab48fbe",
+    "url": "/SortByReact/static/js/main.7b37904f.chunk.js"
+  },
+  {
+    "revision": "83bb32fb90ad5c32455c",
+    "url": "/SortByReact/static/js/runtime-main.d3f6a18f.js"
+  }
+]);
